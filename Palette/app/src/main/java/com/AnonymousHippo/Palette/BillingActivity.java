fbb7f10bed808dd0031c50e39a4a293ac899ab35
Hoeun <37548919+leehe228@users.noreply.github.com>
2020-11-05 16:38:33 +0900

    2020.11.05. 4th
    add Billing Activity

---
+++ Palette/app/src/main/java/com/AnonymousHippo/Palette/BillingActivity.java
@@ -0,0 +1,12 @@
+package com.AnonymousHippo.Palette;
+
+import android.os.Bundle;
+
+public class BillingActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_billing);
+    }
+}
