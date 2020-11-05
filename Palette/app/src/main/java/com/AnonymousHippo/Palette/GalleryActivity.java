528b69f4226ba9aa0b690d0cd13e2d16a8c6bc7b
Hoeun <37548919+leehe228@users.noreply.github.com>
2020-11-05 16:06:40 +0900

    2020.11.05. third
    gallery / other XMLs

--- Palette/app/src/main/java/com/AnonymousHippo/Palette/GalleryActivity.java
+++ Palette/app/src/main/java/com/AnonymousHippo/Palette/GalleryActivity.java
@@ -1,26 +1,118 @@
 package com.AnonymousHippo.Palette;
 
+import android.content.pm.ActivityInfo;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 public class GalleryActivity extends BaseActivity {
 
+    /* 몰입모드 */
+    private View decorView;
+    private int uiOption;
+
+    // 줌 인, 줌 아웃
     private ScaleGestureDetector mScaleGestureDetector;
     private float mScaleFactor = 1.0f;
     private ImageView mainImageView;
+
+    // 저장한 전시회 Flag
+    private boolean SAVED;
+
+    // 화면 회전 Flag
+    private boolean ROTATED;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery);
 
+        View view = getWindow().getDecorView();
+        view.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        getWindow().setStatusBarColor(Color.parseColor("#000000"));
+
+        /* 화면 꺼짐 방지 */
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        setContentView(R.layout.activity_gallery);
+
+        /* 몰입 모드 (하단 소프트바 숨기기) */
+        decorView = getWindow().getDecorView();
+        uiOption = getWindow().getDecorView().getSystemUiVisibility();
+        uiOption |= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        uiOption |= View.SYSTEM_UI_FLAG_FULLSCREEN;
+        uiOption |= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+
         // 인스턴스화
         mainImageView = findViewById(R.id.Gallery_ImageView_main);
 
+        ImageButton leftButton = findViewById(R.id.Gallery_ImageButton_left);
+        ImageButton rightButton = findViewById(R.id.Gallery_ImageButton_right);
+        ImageButton closeButton = findViewById(R.id.Gallery_ImageButton_close);
+        ImageButton fullScreenButton = findViewById(R.id.Gallery_ImageButton_full);
+        final ImageButton plusButton = findViewById(R.id.Gallery_ImageButton_plus);
+
         mScaleGestureDetector = new ScaleGestureDetector(this, new ScaleListener());
+
+        // 초기화
+        SAVED = false;
+        ROTATED = false;
+
+        // Left Button
+        leftButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        // Right Button
+        rightButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        // Close Button
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        // Plus Button
+        plusButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(SAVED){
+                    plusButton.setImageResource(R.drawable.plus);
+                }
+                else{
+                    plusButton.setImageResource(R.drawable.checked);
+                }
+                SAVED = !SAVED;
+            }
+        });
+
+        // Full Screen Button
+        fullScreenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ROTATED){
+                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+                } else{
+                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+                }
+                decorView.setSystemUiVisibility(uiOption);
+                ROTATED = !ROTATED;
+            }
+        });
     }
 
     @Override
@@ -48,6 +140,14 @@ public class GalleryActivity extends BaseActivity {
             mainImageView.setScaleY(mScaleFactor);
 
             return true;
+        }
+    }
+
+    // Background -> Foreground 올라왔을 때 몰입 모드 재적용
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        if (hasFocus) {
+            decorView.setSystemUiVisibility(uiOption);
         }
     }
 }
--- Palette/app/src/main/java/com/AnonymousHippo/Palette/LoadingActivity.java
+++ Palette/app/src/main/java/com/AnonymousHippo/Palette/LoadingActivity.java
@@ -2,6 +2,10 @@ package com.AnonymousHippo.Palette;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class LoadingActivity extends BaseActivity {
 
@@ -10,7 +14,12 @@ public class LoadingActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
 
-        Intent intent = new Intent(getApplicationContext(), GalleryActivity.class);
+        /* 로고 애니메이션 */
+        Animation LogoAnimation = AnimationUtils.loadAnimation(LoadingActivity.this, R.anim.translate);
+        final TextView logoTextView = findViewById(R.id.Loading_TextView_title);
+        logoTextView.startAnimation(LogoAnimation);
+
+        Intent intent = new Intent(getApplicationContext(), WelcomeActivity.class);
         startActivity(intent);
         overridePendingTransition(0, 0);
         finish();
